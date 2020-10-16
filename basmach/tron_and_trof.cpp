b69cea9fc565beedc600017ac1faf154d1c0e0fa
arielofer <bgzahav8@gmail.com>
2020-10-16 13:03:32 +0300

    docs for tron and trof

--- basmach/tron_and_trof.cpp
+++ basmach/tron_and_trof.cpp
@@ -1,5 +1,23 @@
 #include "tnt_funcs.h"
 
+//-----------------------------------------------------------------------------
+// tron & trof
+// -----------
+//
+// General : The program builds the tron planet and moves the trof
+//			 around according to user input.
+//
+// Input : number of trons.
+//
+// Process : insert number of trons to the matrix according to user input
+//			 coordinets, then according to user input scan the matrix.
+//
+// Output : Prints status of trof in the end of the scan(dead or tired).
+//
+//-----------------------------------------------------------------------------
+// Programmer : Ariel Ofer
+// Date : 13.10.2020
+//-----------------------------------------------------------------------------
 int main()
 {
 	int board[N * 2 + 2][N + 2] = { 0 }, num_of_trons;
